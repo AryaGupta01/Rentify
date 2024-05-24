c9536df708610c9482a0274ffe7e82ec7c0032ea
Arya Gupta <141574060+AryaGupta01@users.noreply.github.com>
2024-05-24 19:33:16 +0000

    upload

---
+++ client/src/firebase.js
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAZJyEmMz1TkYoCG37EWmAr49tBEmgksxs",
+  authDomain: "novahorizon-91d6b.firebaseapp.com",
+  projectId: "novahorizon-91d6b",
+  storageBucket: "novahorizon-91d6b.appspot.com",
+  messagingSenderId: "1060899398186",
+  appId: "1:1060899398186:web:194a96099c4b3d41e05953",
+};
+
+export const app = initializeApp(firebaseConfig);
